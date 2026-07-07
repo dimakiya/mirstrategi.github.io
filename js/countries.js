20e1ad214ed5e6ed34a09c80a8e2fc4242e31437
dimakiya <dimas13st@gmail.com>
2026-07-07 10:10:08 +0500

    Create countries.js

---
+++ js/countries.js
@@ -0,0 +1,34 @@
+class Country {
+  constructor(id, name, color, flagUrl) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    this.flagUrl = flagUrl;
+    this.population = 0;
+    this.money = 0;
+    this.army = 0;
+    this.regions = [];
+    this.alliances = [];
+    this.activeWars = [];
+  }
+
+  changeName(newName) {
+    this.name = newName;
+  }
+
+  changeColor(newColor) {
+    this.color = newColor;
+  }
+
+  changeFlag(newFlagUrl) {
+    this.flagUrl = newFlagUrl;
+  }
+}
+
+const COUNTRY_TEMPLATES = {
+  ESP: { name: "Испания", color: "#E8000D", flag: "assets/images/flags/spain.png" },
+  PRT: { name: "Португалия", color: "#006633", flag: "assets/images/flags/portugal.png" },
+  AND: { name: "Андорра", color: "#2E57A4", flag: "assets/images/flags/andorra.png" }
+};
+
+export { Country, COUNTRY_TEMPLATES };
